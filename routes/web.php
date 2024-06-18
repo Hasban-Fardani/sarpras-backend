6ac7179f63a1318f12bb6e975f9e8aea6cbb389a
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-06-18 18:11:39 +0700

    fix: vercel function oversize

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,4 @@
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
-
 Route::post('/login', LoginController::class);
-
