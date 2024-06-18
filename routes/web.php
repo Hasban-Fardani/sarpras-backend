f4c6ffc5cb645aa165e461ff26610027a75ccdbf
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-06-18 18:21:15 +0700

    fix: vercel not found

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return ['laravel'=>app()->version()];
+});
+
 Route::post('/login', LoginController::class);
