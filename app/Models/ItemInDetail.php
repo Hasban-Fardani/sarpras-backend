2a9b503e07bb8388ea03d7f710d5fe96f895b2ad
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-07-23 14:44:21 +0700

    feat(item-in): store, get and delete item in

--- app/Models/ItemInDetail.php
+++ app/Models/ItemInDetail.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class ItemInDetail extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function item()
+    {
+        return $this->belongsTo(Item::class);
+    }
+
+    public function item_in()
+    {
+        return $this->belongsTo(ItemIn::class);
+    }
 }
